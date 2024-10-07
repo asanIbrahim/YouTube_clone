7aa103f1bd928aa2deffd3cb517b05e75eeebcaf
asanibrahim <asanib98@gmail.com>
2024-10-07 23:55:09 +0530

    like

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -14,7 +14,7 @@ export const Sidebar = () => {
       </div>
       <div className="sidebaer_button">
         <HomeIcon />
-        Homes
+        Home
       </div>
       <div className="sidebaer_button">
         <OndemandVideoIcon />
