9e8bea53c636f6c4fbf082c6424b68886fb20720
asanIbrahim <asanib98@gmail.com>
2025-01-16 08:19:16 +0530

    rename home icon

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -14,7 +14,7 @@ export const Sidebar = () => {
       </div>
       <div className="sidebaer_button">
         <HomeIcon />
-        Home
+        Homes
       </div>
       <div className="sidebaer_button">
         <OndemandVideoIcon />
