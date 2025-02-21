5f6d61a9f87546c57d364bcf29d3c5baed05bc25
asanIbrahim <asanib98@gmail.com>
2025-02-21 08:42:03 +0530

    change css

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -14,19 +14,19 @@ export const Sidebar = () => {
       </div>
       <div className="sidebaer_button">
         <HomeIcon />
-        Home
+        Homes
       </div>
       <div className="sidebaer_button">
         <OndemandVideoIcon />
-        Short
+        Shorts
       </div>
       <div className="sidebaer_button">
         <PlayCircleIcon />
-        Subscription
+        Subscriptions
       </div>
       <div className="sidebaer_button">
         <PlayArrowIcon />
-        You like
+        You likes
       </div>
     </div>
   );
