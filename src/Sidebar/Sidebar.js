e90f8f3f82872d1f04ee3583b5021d8272920ff6
asanIbrahim <asanib98@gmail.com>
2025-04-13 18:20:57 +0530

    add useReducer

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -21,7 +21,7 @@ export const Sidebar = () => {
       </div>
       <div className="sidebaer_button">
         <PlayCircleIcon />
-        Subscriptionsss
+        Subscriptionss
       </div>
       <div className="sidebaer_button">You like</div>
     </div>
