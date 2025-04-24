8b735856d9d36247132be055d3d33808d9fd8733
asanIbrahim <asanib98@gmail.com>
2025-04-24 18:43:44 +0530

    add dynamic value

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -13,7 +13,7 @@ export const Sidebar = () => {
       </div>
       <div className="sidebaer_button">
         <HomeIcon />
-        Home
+        Ho
       </div>
       <div className="sidebaer_button">
         <OndemandVideoIcon />
